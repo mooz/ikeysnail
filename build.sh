2c921d26d5e4adb6618510556ec87084fc2ae19f
Masafumi Oyamada <stillpedant@gmail.com>
2019-07-29 15:57:23 +0000

    Change build.sh to prettify main.js

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-prettier --write **/*.js
+prettier --tab-width 2 --write main.js
+prettier --tab-width 2 --write **/*.js
 uglifyjs --comments -- strings/content-script.js > strings/content-script.min.js
 # uglifyjs -- scripts/settings.js > scripts/settings.min.js
