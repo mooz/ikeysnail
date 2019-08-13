0e8c95691f939ed615345b570e0b011df71979bb
Masafumi Oyamada <stillpedant@gmail.com>
2019-08-13 22:16:49 +0900

    Change build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 prettier --tab-width 2 --write main.js
 prettier --tab-width 2 --write **/*.js
-uglifyjs --comments -- strings/content-script.js > strings/content-script.min.js
+# uglifyjs --comments -- strings/content-script.js > strings/content-script.min.js
 # uglifyjs -- scripts/settings.js > scripts/settings.min.js
