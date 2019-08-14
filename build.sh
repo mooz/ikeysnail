09ed2afefe0591c967151b66a8421c2b947f4dc7
Masafumi Oyamada <stillpedant@gmail.com>
2019-08-14 11:24:26 +0900

    Fix prettier command to correctly handle recursive glob

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-prettier --tab-width 2 --write main.js
-prettier --tab-width 2 --write **/*.js
+prettier --tab-width 2 --write 'src/**/*.js'
 # uglifyjs --comments -- strings/content-script.js > strings/content-script.min.js
 # uglifyjs -- scripts/settings.js > scripts/settings.min.js
