5afd2e39bfb3a893dcef212baa872616f396f795
Masafumi Oyamada <stillpedant@gmail.com>
2019-08-15 13:17:51 +0900

    Add node_modules to webpack module directory

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 	  plugins: [new webpack.ProgressPlugin()],
 
     resolve: {
-        modules: ["./src"],
+        modules: ["./src", "./node_modules"],
     },
 
 	  module: {
